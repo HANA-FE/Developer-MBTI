fa7ae3c26d2b717c7c0ab69c0713505201e1d7a4
lucy <lucyp01330@gmail.com>
2025-03-17 16:24:38 +0900

    feature: 홈 화면 초안, index.css에 초안 넣음

--- src/component/UI/Button.jsx
+++ src/component/UI/Button.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import "./Button.css";
 
 function Button({ text, onClick, type }){
-    
     return(
         <button className={type} onClick={onClick}>{text}
         </button>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div>Home</div>;
+import React from 'react';
+import '../../../style/index.css';
+
+function Home(){
+  return(
+<div>
+  <div className="title1">
+    <p>개발놈 TEST</p>
+  </div>
+
+  <div className="title2">
+    <p>당신은 어떤 개발 동물일까요?</p>
+  </div>
+  
+  <div className="cat">
+  <img src= "Cat.png" alt="Cat"/>
+  </div>
+  
+  <div className="screen">
+  <img src= "screen.png" alt="screen"/>
+  </div>
+
+</div>
+  );
 }
+
+export default Home;
