1807de62b46af1d1c3d8daa7d91e38f5342f97ea
lucy <lucyp01330@gmail.com>
2025-03-17 15:00:06 +0900

    버튼 수정 완료

--- src/component/UI/Button.jsx
+++ src/component/UI/Button.jsx
@@ -3,10 +3,8 @@ import "./Button.css";
 
 function Button({ text, onClick, type }){
     
-    const buttonClass = type == "start" ? "start-button" : "answer-button";
-
     return(
-        <button className={buttonClass} onClick={onClick}>{테스트 시작하기}
+        <button className={type} onClick={onClick}>{text}
         </button>
     );
 }
