556dbdab6ae10365504ed7d73032dcf973c57043
kiv9908 <leebongwook0805@gmail.com>
2025-03-18 11:45:26 +0900

    feat: test page 접근 url(/test) 추가

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,5 @@
 import Test from './Test';
 
 export default function Home() {
-  // return <div>Home</div>;
-  return <Test></Test>;
-  
+  return <div>Home</div>;
 }
--- src/page/Route.jsx
+++ src/page/Route.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes as ReactRouterRoutes } from "react-router-dom";
-import Home from "./Home";
+import { Route, Routes as ReactRouterRoutes } from 'react-router-dom';
+import Home from './Home';
+import Test from './Test';
 
 export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="*" element={<Home />} />
+      <Route path="/test" element={<Test />} />
     </ReactRouterRoutes>
   );
 }
