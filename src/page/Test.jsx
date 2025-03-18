91690532dd16ba0fd8006101487a736993fdea84
kiv9908 <leebongwook0805@gmail.com>
2025-03-18 13:23:40 +0900

    fix: Question.js 전체구조, answer 배열로 수정

--- src/page/Test.jsx
+++ src/page/Test.jsx
@@ -2,8 +2,10 @@ import Button from '../component/UI/Button';
 import '../../style/index.css';
 import QuestionText from '../component/UI/QuestionText';
 import QuestionImage from '../component/UI/QuestionImage';
+import QUESTION from '../constant/Question';
+
 const Test = () => {
-  const id = 12;
+  const id = 1;
   return (
     <div className="test-page">
       {/* 질문 영역 */}
@@ -12,8 +14,14 @@ const Test = () => {
       </div>
 
       {/* 이미지 영역 */}
-      <div className="image-wrapper">
+      <div className="question-image-wrapper">
         <QuestionImage questionId={id} />
+      </div>
+
+      {/* 버튼 영역 */}
+      <div className="test-button-wrapper">
+        <Button text={QUESTION[id].answers[0].text} type="test-button" />
+        <Button text={QUESTION[id].answers[1].text} type="test-button" />
       </div>
     </div>
   );
